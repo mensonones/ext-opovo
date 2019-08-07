4de8340161d9007f0c58f4846767402ec703005a
mensonones <mensones.1@gmail.com>
2019-08-07 16:27:11 -0300

    insert code to close banner page

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,7 @@
 window.onload = function() {
+  let banner = document.getElementsByClassName("close-modal")[0];
+  banner.click();
+
   document.getElementsByClassName(
     "container conteudo interna"
   )[0].style.display = "block";
